3bda8c322d453219c8d32ce77ba4a3d3010301b9
Richard Clark <rdclark@nextquestion.net>
2010-10-27 20:48:30 -0700

    Building the board, passing the tests

--- src/board.js
+++ src/board.js
@@ -6,45 +6,125 @@
  * Factory method.
  */
 var board = function(spec) {
-	var that = {serial_number: null};
-	spec = spec || {};
-	var size = spec.size || 3;
-	var correctValues = spec.correctValues || [];
-	var distractors = spec.distractors || [];
-	var freeCellLabel = spec.freeCellLabel || "Free space";
-
-	that.is_winning_card = spec.is_winning_card || true;
-
+	var that = {};
+	
+	var packageValues = function(values, correct) {
+		if (values == null) return [];
+		return values.map(function(x) { return {value:x, correct:correct} });
+	};
+	
 	var init = function(size) {
-		var cells = [];
+		var c = [];
 		if (size % 2 == 0) throw new Error("only odd-sized arrays supported");
-		var numCells = size * size;
-		for (var i=0; i < numCells; i++) {cells[i]={value:null}};
-		cells[(numCells - 1) / 2] = {value:freeCellLabel, correct: true}; // free cell in center
-		return cells;
+		for (var i=0; i < numCells; i++) {c[i]={value:null, correct:false}};
+		var mp = {};
+		mp.value = freeCellLabel;
+		mp.correct = true;
+		c[midpoint] = mp; // free cell in center
+		return c;
 	};	
 	
 	var build = function() {
 		if (size == 1) {
 			that.is_winning_card = true;
-			that.serial_number = "0";
 			// winningLine = 0;
 			// winningCells = [0];
-		} else {
-			// if (is_winning_card) addCorrect();
+		} else if (distractors.length > 0 && correct_values.length > 0) {
+			var lines = buildLines();
+			if (that.is_winning_card) addWinningLine(lines);
+			addDistractors(lines);
+			fillRemainingCells();
 		}
 		return that;
 	};
 	
-	that.get_cells = function() { cells.slice() };
+	var buildLines = function() {
+		var i = 0;
+		var row, col;
+		var lines = [];
+		for (row = 0; row < size; row++) {
+			var entry = [];
+			for (col = 0; col < size; col++) { entry.push(i++) }
+			lines.push(entry);
+		}
+		for (col = 0; col < size; col++) { 
+			entry = [];
+			for (row = 0; row < size; row++) { entry.push(col + row * size) }
+			lines.push(entry);
+		}
+		// left diagonal
+		var ld = [];
+		var rd = [];
+		for (row = 0; row < size; row++) { ld.push(row + row * size); rd.push(size-row-1 + row * size) }
+		lines.push(ld, rd);
+		return lines;
+	};
+	
+	var setCell = function(index, list) {
+		if (index != midpoint && (that.cells[index] == null || that.cells[index].value == null)) that.cells[index] = list.splice(0,1)[0];
+	};
+	
+	var addWinningLine = function(lines) {
+		var line = extractRandomElement(lines);
+		that.winning_cells = line;
+		for (var i=0; i < size; i++) setCell(line[i], correct_values);
+	};
+	
+	var rand = function(n) { return Math.floor(Math.random() * n) };
+	
+	var extractRandomElement = function(list) { return list.splice(rand(list.length),1)[0] };
+	
+	var getRandomElement = function(list) { return list[rand(list.length)] };
+	
+	var addDistractors = function(lines) {
+		var i;
+		while (lines.length > 0) {
+			var line = extractRandomElement(lines);
+			if ((i=line.indexOf(midpoint)) >= 0) {line.splice(i,1)}; // no midpoint
+			i = getRandomElement(line);
+			setCell(i, distractors);
+			lines = lines.filter(function(cells){return cells.indexOf(i)<0}); // remove all lines that contain this cell
+		}
+	};
+	
+	var fillRemainingCells = function() {
+		var remainder = shuffle(correct_values.concat(distractors));
+		for (var i=0;i<numCells;i++) {
+ 			 if (that.cells[i] == null || that.cells[i].value == null) setCell(i, remainder);
+		}
+	};
+	
+	
+	var shuffle = function(o){
+		for(var j, x, i = o.length; i; j = Math.floor(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
+		return o;
+	};
+
+	that.get_cells = function() { return that.cells.slice(0) };
 
 	that.cell_at = function(row, col) { 
 		var i = row * size + col;
-		return (i < 0 || i >= cells.length) ? null : cells[i];
+		return (i < 0 || i >= that.cells.length) ? null : that.cells[i];
 	};
 		
+	
+	// Initialize values
+	spec = spec || {};
+	var size = spec.size || 3;
+	var numCells = size * size;
+	var midpoint = (numCells - 1) / 2;
+	var correct_values = packageValues(spec.correct_values, true);
+	var distractors = packageValues(spec.distractors, false);
+	var freeCellLabel = spec.freeCellLabel || "Free space";
+	
+	shuffle(correct_values);
+	shuffle(distractors);
+
+	that.is_winning_card = (spec.is_winning_card == null) ? true : spec.is_winning_card;
+	that.winning_cells = null;
+
 	// Create a blank board
-	var cells = init(size);
+	that.cells = init(size);
 	build();
 	return that;
 };
--- test/board_test.js
+++ test/board_test.js
@@ -37,8 +37,72 @@ test("center cell is free space", function() {
 test("single-cell card is a winning card", function() {
 	var b = board({size:1});
 	var c0 = b.cell_at(0,0);
-	expect(3);
+	expect(2);
 	ok(c0.correct, "Free space is correct");
 	ok(b.is_winning_card, "is a winning card");
-	equals(b.serial_number, "0", "serial number == 0");
 });
+
+test("winning card has a correct row", function() {
+	var correct_values =['c1','c2','c3','c4','c5'];
+	var incorrect_values=['d1', 'd2', 'd3'];
+	var b = board({size:3, is_winning_card: true, correct_values:correct_values, distractors:incorrect_values});
+	var c0 = b.cell_at(1,1);
+	expect(4);
+	ok(c0.correct, "Free space is correct");
+	ok(b.is_winning_card, "Is a winning card");
+	ok(b.winning_cells != null, "Has winning cells");
+	equals(b.winning_cells.length, 3, "Has three winning cells");
+//	ok(b.get_cells().every(function(x){x != null && x.value != null}), "Every cell has a value");
+});
+
+test("non-winning card has no correct row", function() {
+	var correct_values =['c1','c2','c3','c4','c5'];
+	var incorrect_values=['d1', 'd2', 'd3'];
+	var b = board({size:3, is_winning_card: false, correct_values:correct_values, distractors:incorrect_values});
+	var c0 = b.cell_at(1,1);
+	expect(3);
+	ok(c0.correct, "Free space is correct");
+	ok(!b.is_winning_card, "Is not a winning card");
+	equals(b.winning_cells, null, "No winning cells");
+});
+
+test("winning card has all cells filled", function() {
+	var correct_values =['c1','c2','c3','c4','c5'];
+	var incorrect_values=['d1', 'd2', 'd3'];
+	var b = board({size:3, is_winning_card: true, correct_values:correct_values, distractors:incorrect_values});
+	expect(1);
+	var cells = b.get_cells();
+	ok(cells.every(function(x){return x != null && x.value != null}), "Every cell has a value");
+});
+
+test("non-winning card has all cells filled", function() {
+	var correct_values =['c1','c2','c3','c4','c5'];
+	var incorrect_values=['d1', 'd2', 'd3'];
+	var b = board({size:3, is_winning_card: false, correct_values:correct_values, distractors:incorrect_values});
+	expect(1);
+	var cells = b.get_cells();
+	ok(cells.every(function(x){return x != null && x.value != null}), "Every cell has a value");
+});
+
+
+/*
+test("card contains correct and incorrect values", function() {
+	var correct_values =['c1','c2','c3','c4','c5'];
+	var incorrect_values=['d1', 'd2', 'd3'];
+	var b = board({size:3, correct_values:['c1','c2','c3','c4','c5'], distractors:['d1', 'd2', 'd3']});
+	var cellValues = b.get_cells().map(function (x){return x.value});
+	expect(10);
+	ok(b.cell_at(1,1).correct, "Free space is correct");
+	ok(b.is_winning_card, "is a winning card");
+	for (var i=0; i<correct_values.length; i++) {
+		var cv = correct_values[i]
+		ok(cellValues.indexOf(cv) >= 0, "contains " + cv);
+	}
+	for (var i=0; i<incorrect_values.length; i++) {
+		var iv = incorrect_values[i]
+		ok(cellValues.indexOf(iv)  >= 0, "contains " + iv);
+	}
+});
+*/
+
+

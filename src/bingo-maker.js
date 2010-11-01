dbe6af0fb056a938822644f5cd25cecead0818bd
Richard Clark <rdclark@nextquestion.net>
2010-10-31 23:32:04 -0700

    Add form-reading function

--- src/bingo-maker.js
+++ src/bingo-maker.js
@@ -6,13 +6,26 @@
  * To change this template use File | Settings | File Templates.
  */
 function buildSample(divID) {
-    var correct_values = ['c1','c2','c3','c4','c5', 'c6', 'c7','c8','c9', 'c10', 'c11','c12','c13','c14','c15', 'c16', 'c17','c18','c19', 'c20'];
-    var incorrect_values = ['d1', 'd2', 'd3', 'd4', 'd5'];
+    var correctValues = ['c1','c2','c3','c4','c5', 'c6', 'c7','c8','c9', 'c10', 'c11','c12','c13','c14','c15', 'c16', 'c17','c18','c19', 'c20'];
+    var incorrectValues = ['d1', 'd2', 'd3', 'd4', 'd5'];
     var size = 5;
-    var b = board({size:size, is_winning_card: true, correct_values:correct_values, distractors:incorrect_values});
+    var b = board({size:size, is_winning_card: true, correctValues:correctValues, incorrectValues:incorrectValues});
     var cells = b.get_cells();
     var table = buildTable(size, cells);
     $(divID).empty().append(table);
+}
+
+function getValuesFromForm() {
+    var size = $('#sizeMenu').val();
+    var numCards = $('#numCards').val();
+    var winningPercent = $('#winningPercent').val();
+    var correctValues = extractStrings($('#correctValues').val());
+    var incorrectValues = extractStrings($('#incorrectValues').val());
+    return {size:size, numCards:numCards, winningPercent:winningPercent, correctValues:correctValues, incorrectValues:incorrectValues};
+}
+
+function extractStrings(textAreaValue) {
+   return textAreaValue.split('\n').map(function (s) { return s.replace(/^\s\s*/, '').replace(/\s\s*$/, '')}).grep(function (s) {return s.length > 0});
 }
 
 function buildTable(size, cells) {
